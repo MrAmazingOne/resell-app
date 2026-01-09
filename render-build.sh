1c3ead6d3629cd959cb730aa6bfe1c5f1f17263a
MrAmazingOne <justindastar@gmail.com>
2026-01-09 11:24:44 -0600

    pussyyyyfartpoop32

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Install system dependencies required for Pillow
+# Install system dependencies required for Pillow and OpenCV
 apt-get update
-apt-get install -y libjpeg-dev zlib1g-dev
+apt-get install -y libjpeg-dev zlib1g-dev libfreetype6-dev libopencv-dev python3-opencv
